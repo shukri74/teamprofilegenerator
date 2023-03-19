e66028d0cd49723af41813033571b91c4d66c259
shukri74 <shukri.mohamed@mail.com>
2023-03-19 22:20:02 +0000

    created employee and engineer classes

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+
+    constructor(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+
+    }
+
+    getName(){
+        return this.name
+    }
+
+    GetId(){
+        return this.id
+    }
+
+    getEmail(){
+        return this.id
+    }
+    
+    getRole(){
+        return 'Employee'
+    }
+    
+};
+
+module.exports = Employee;
+
